ecab6b86151050ad5bce9e891e4a6d13270d9a8d
Yovenko Artur <46196803+Ononim@users.noreply.github.com>
2020-03-24 21:06:32 +0200

    Add files via upload

---
+++ fourierlabs.py
@@ -0,0 +1,60 @@
+import matplotlib.pyplot as plt
+from math import *
+from cmath import exp, pi
+import random
+
+W, N, HARM, AMPL, SHIFT = 2000, 256, 8, random.uniform(0, 1), random.uniform(0, 1)
+
+
+def get_signal_point(t, w, ampl, sh):
+    return sin(w * t + sh) * ampl
+
+
+def get_dft(n):
+    res = list()
+    for i in range(len(n)):
+        s = 0
+        for j in range(len(n)):
+            s += n[j] * complex(cos(-2 * pi * j * i / len(n)), sin(-2 * pi * j * i / len(n)))
+        res.append(s)
+    return res
+
+
+def get_fft(n):
+    l = len(n)
+    if l <= 1: return n
+    even = get_fft(n[0::2])
+    odd = get_fft(n[1::2])
+    t = [exp(-2j * pi * i / l) * odd[i] for i in range(l // 2)]
+    return [even[i] + t[i] for i in range(l // 2)] + [even[i] - t[i] for i in range(l // 2)]
+
+
+def get_real(arr):
+    return [i.real for i in arr]
+
+
+def get_imag(arr):
+    return [i.imag for i in arr]
+
+
+if __name__ == "__main__":
+    signal = list()
+
+    for i in range(N):
+        s = 0
+        for j in range(HARM):
+            s += get_signal_point(i, W * j / HARM, AMPL, SHIFT)
+        signal.append(s)
+
+    plt.subplot(221)
+    plt.ylabel('DFT')
+    plt.plot(get_real(get_dft(signal)))
+    plt.subplot(222)
+    plt.plot(get_imag(get_dft(signal)))
+    plt.subplot(223)
+    plt.ylabel('FFT')
+    plt.plot(get_real(get_fft(signal)), "red")
+    plt.subplot(224)
+    plt.plot(get_imag(get_fft(signal)), "red")
+
+    plt.savefig("plot.png")
